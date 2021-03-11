0521fd14884f826c45ed7a32ac9e1610409275da
Martin Lindén <martin.linden.k@gmail.com>
2021-03-11 10:42:43 +0100

    Added booking subclasses

---
+++ PTBooking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_projekt1
+{
+    class PTBooking : Booking
+    {
+        private PersonalTrainer _PT;
+        public PTBooking(PersonalTrainer PT, int bookingID, DateTime time, Member member) : base(bookingID, time, member)
+        {
+            _PT = PT;
+        }
+    }
+}
