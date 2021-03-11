f17499c0db25245fc9707e596d0ac1960bc76531
Erik <erik.appelgren@hotmail.com>
2021-03-11 10:25:00 +0100

    Register metod

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OOP_projekt1
 {
     class Program
     {
+        public List<Member> Members = new List<Member>();
         static void Main(string[] args)
         {
-            Member testObj = new Member(01, "Erik", "Appelgren", "1234", "072");
-            Admin test = new Admin(02, "odksaf", "apa", "4345", "1234566");
-            
-            Console.WriteLine(testObj.FirstName);
-            Console.WriteLine(test.UserID);
+            Register("odksaf", "apa", "4345", "1234566");
+        }
+        static void Register(string fn, string ln, string pw, string pn)
+        {
+            List<Member> Members = new List<Member>();
+            int userID = Members.Count+1;
+            Member newMember = new Member(userID, fn, ln, pw, pn);
+            Members.Add(newMember);
+            foreach (Member item in Members)
+            {
+                Console.WriteLine(newMember.UserID);
+                Console.WriteLine(newMember.FirstName);
+                Console.WriteLine(newMember.LastName);
+                Console.WriteLine(newMember.PassWord);
+                Console.WriteLine(newMember.PhoneNumber);
+            }
         }
     }
 }
