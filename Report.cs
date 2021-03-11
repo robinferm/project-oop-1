996c412e39b964391a3a2fa23a485f151edf70a7
Robin Ferm <robinferm@live.com>
2021-03-11 15:29:05 +0100

    equipment and report class

---
+++ Report.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_projekt1
+{
+    class Report : Equipment
+    {
+        private string _description;
+        private ActionType _action;
+
+        public enum ActionType
+        {
+            Replace,
+            Repair
+        }
+
+        public Report(string description, ActionType action, int equipmentID, EquipmentType equipmentType): base(equipmentID, equipmentType)
+        {
+            _description = description;
+            _action = action;
+        }
+    }
+}
